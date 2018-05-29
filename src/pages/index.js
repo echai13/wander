6aedabe999851eb57abc32b18f14ac8060822377
echai13 <cher13@brandeis.edu>
2018-05-29 00:58:20 -0400

    transitions

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,42 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
-export default class IndexPage extends React.Component {
+import clothespin from '../img/clothespin.svg';
+import albumOne from '../../static/img/chemex.jpg';
+import albumTwo from '../../static/img/coffee.png';
+import albumThree from '../../static/img/meeting-space.png';
 
+export default class IndexPage extends React.Component {
+  scrollPos = 0;
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
   }
 
   handleScroll = () => {
+    if (window.scrollY < this.scrollPos) {
+      document.querySelector('div.nav-menu').style.display = 'block';
+      document.querySelector('div.top-section').style.top = '5vh';
+    }
+
     console.log('scrollPos: ', window.scrollY);
     if (window.scrollY > 30) {
       document.querySelector('div.top-section > h1').classList.add('slide-up');
+      document.querySelector('div.top-section').classList.add('slide-up');
+      document.querySelector('div.nav-menu').classList.add('slide-up');
+
+      if (scrollY > this.scrollPos && window.scrollY > 100) {
+        document.querySelector('div.nav-menu').style.display = 'none';
+        document.querySelector('div.top-section').style.top = '0';
+      }
     }
+
+    if (window.scrollY > 520 && window.scrollY < 800) {
+      document.querySelector('div.nav-menu').style.color = 'white';
+    } else if (window.scrollY > 800 || window.scrollY < 520) {
+      document.querySelector('div.nav-menu').style.color = 'black';
+    }
+
+    this.scrollPos = window.scrollY;
   }
 
   renderMenuDetails = () => {
@@ -39,18 +64,49 @@ export default class IndexPage extends React.Component {
             <h1>wander</h1>
           </div>
 
-          <div className="featured-image" />
+          <div className="featured-image">
+            <div className="featured-title">
+              Trip to Rome, Italy
+            </div>
+          </div>
 
           <div className="featured-posts">
             {
               posts.map(post => (
-                <div>
-                  <h3>{post.node.frontmatter.title}</h3>
-                  <div>{post.node.excerpt}</div>
-                  <img src={post.node.frontmatter.image || ''} />
+                <div className="ui two column doubling stackable grid container">
+                  <div className="column">
+                    <div className="image-wrapper">
+                      <img src={post.node.frontmatter.image || ''} />
+                    </div>
+                  </div>
+                  <div className="column">
+                    <h3>{post.node.frontmatter.title}</h3>
+                    <div>{post.node.excerpt}</div>
+                  </div>
                 </div>
               ))
             }
+          </div>
+
+          <div className="albums">
+            {/* <img src={clothespin} className="clothespin" /> */}
+            <div className="ui three column doubling stackable grid container">
+              <div className="column">
+                <div className="ui stacked segment album">
+                  <img src={albumOne} />
+                </div>
+              </div>
+              <div className="column">
+                <div className="ui stacked segment album">
+                  <img src={albumTwo} />
+                </div>
+              </div>
+              <div className="column">
+                <div className="ui stacked segment album">
+                  <img src={albumThree} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
